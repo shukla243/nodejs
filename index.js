70ab8ff41e650025b81e75c14812e897cad79633
mails4shukla <111882432+mails4shukla@users.noreply.github.com>
2022-08-23 10:09:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World- version4';
+  const name = process.env.NAME || 'World- version6';
   res.send(`Hello ${name}!`);
 });
 
