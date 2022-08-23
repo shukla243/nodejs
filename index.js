6aac01c55de619b51724d80ad02dc03565fa315c
mails4shukla <111882432+mails4shukla@users.noreply.github.com>
2022-08-23 09:19:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World- version5';
+  res.send(`Hello ${name}!`);
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
